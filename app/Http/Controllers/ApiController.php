9e6c8523f633d5d6945d27732513deb82d8b979c
Diana <diana.cuenca.zambrano@udla.edu.ec>
2023-04-17 20:14:27 -0500

    first commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ApiController extends Controller
+{
+    public function login($nombreUsuario, $passwordUsuario, $codigoEmisor)
+    {
+        try {
+            $apiURL = getenv('API_SERVICIOS');
+            $url = $apiURL . '/api/Usuarios?usuario=' . $nombreUsuario . '&password=' . $passwordUsuario;
+
+            $response = Http::get($url);
+            $aux = (string) $response->getBody();
+
+            if (strcmp($aux, "error") == 0) {
+                return response()->json(['success' => 0, 'message' => 'Existen errores en los datos ingresados']);
+            }
+            
+            if ($response[0]['Emisor'] != $codigoEmisor) {
+                return response()->json(['success' => 0, 'message' => 'El codigo del Emisor no coincide']);
+            }
+
+            if ($response[0]['OBSERVACION'] === 'CONTRASEÑA INVALIDA') {
+                return response()->json(['success' => 0, 'message' => 'Contraseña invalida']);
+            }
+
+            return response()->json(['success' => 1, 'message' => $response]);
+        } catch (\Throwable $th) {
+            return response()->json(['success' => 1, 'message' => $th]);
+        }
+    }
+}
