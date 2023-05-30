477c1f76a729aa0c6e40156931f58499b03e2798
Diana <diana.cuenca.zambrano@udla.edu.ec>
2023-05-30 01:19:37 -0500

    mov lan upd e ins

--- app/Http/Controllers/MovimientoPlantillaController.php
+++ app/Http/Controllers/MovimientoPlantillaController.php
@@ -27,22 +27,21 @@ class MovimientoPlantillaController extends Controller
 
     public function insertMovimientoPlanilla(Request $request)
     {
-        //try {
+        try {
             $apiURL = 'http://apiservicios.ecuasolmovsa.com:3009';
             $url = $apiURL . '/api/Varios/MovimientoPlanillaInsert?conceptos='.$request->conceptos.'&prioridad='.$request->prioridad.'&tipooperacion='.$request->tipooperacion.'&cuenta1='.$request->cuenta1.'&cuenta2='.$request->cuenta2.'&cuenta3='.$request->cuenta3.'&cuenta4='.$request->cuenta4.'&MovimientoExcepcion1='.$request->MovimientoExcepcion1.'&MovimientoExcepcion2='.$request->MovimientoExcepcion2.'&MovimientoExcepcion3='.$request->MovimientoExcepcion3.'&Traba_Aplica_iess='.$request->Traba_Aplica_iess.'&Traba_Proyecto_imp_renta='.$request->Traba_Proyecto_imp_renta.'&Aplica_Proy_Renta='.$request->Aplica_Proy_Renta.'&Empresa_Afecta_Iess='.$request->Empresa_Afecta_Iess;
             //return $url;
             $response = Http::get($url);
             $response = $response->getBody();
             return response()->json(['success' => 1, 'message' => $response, 'url' => $url], 200);
-            return $response;
             // if ($response == '') {
             //     return response()->json(['success' => 0, 'message' => 'El Centro de Costo ya existe'], 201);
             // } else {
             //     return response()->json(['success' => 1, 'message' => $response[0]], 200);
             // }
-        /* } catch (\Exception $th) {
+        } catch (\Exception $th) {
             return response()->json(['success' => 0, 'message' => $th], 201);
-        } */
+        }
     }
 
     public function updateMovimientoPlanilla(Request $request)
@@ -67,14 +66,14 @@ class MovimientoPlantillaController extends Controller
             '&Empresa_Afecta_Iess='.$request->Empresa_Afecta_Iess;
 
             $response = Http::get($url);
-            return response()->json(['success' => 1, 'message' => $response[0]['NombreCentroCostos']], 200);
+            return response()->json(['success' => 1, 'message' => $response], 200);
             /* if ($response[0]['Codigo'] != null && $response[0]['NombreCentroCostos'] != 'Actualizacíón Correcta') {
                 return response()->json(['success' => 0, 'message' => 'No se pudo actualizar'], 201);
             } else {
                 return response()->json(['success' => 1, 'message' => $response[0]['NombreCentroCostos']], 200);
             } */
         } catch (\Exception $th) {
-            return response()->json(['success' => 0, 'message' => '$th'], 201);
+            return response()->json(['success' => 0, 'message' => $th], 201);
         }
     }
 
