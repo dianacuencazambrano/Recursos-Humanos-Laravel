72c8d4c709a0578c810590258a9c23749f25d283
Diana <diana.cuenca.zambrano@udla.edu.ec>
2023-06-13 18:50:53 -0500

    responses

--- app/Http/Controllers/TrabajadorController.php
+++ app/Http/Controllers/TrabajadorController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Controller;
@@ -11,31 +10,31 @@ class TrabajadorController extends Controller
 {
     public function getTrabajador(Request $request)
     {
-        //try {
+        try {
             $apiURL = 'http://apiservicios.ecuasolmovsa.com:3009';
             $url = $apiURL . '/api/Varios/TrabajadorSelect?sucursal='.$request->sucursal;
             $response = Http::get($url);
             $response = $response->getBody();
             return $response;
             //return response()->json(['success' => 1, 'message' => $response[0]], 200);
-        /* } catch (\Exception $th) {
+        } catch (\Exception $th) {
             return response()->json(['success' => 0, 'message' => $th], 201);
-        } */
+        }
     }
     public function insertTrabajador(Request $request)
     {
-        //try {
+        try {
             $apiURL = 'http://apiservicios.ecuasolmovsa.com:3009';
             //$url = $apiURL . '/api/Varios/TrabajadorInsert?COMP_Codigo='.$request->COMP_Codigo.'&Tipo_trabajador='.$request->Tipo_trabajador.'&Apellido_Paterno='.$request->Apellido_Paterno.'&Apellido_Materno='.$request->Apellido_Materno.'&Nombres='.$request->Nombres.'&Identificacion='.$request->Identificacion.'&Entidad_Bancaria='.$request->Entidad_Bancaria.'&CarnetIESS='.$request->CarnetIESS.'&Direccion='.$request->Direccion.'&Telefono_Fijo='.$request->Telefono_Fijo.'&Telefono_Movil='.$request->Telefono_Movil.'&Genero='.$request->Genero.'&Nro_Cuenta_Bancaria='.$request->Nro_Cuenta_Bancaria.'&Codigo_Categoria_Ocupacion='.$request->Codigo_Categoria_Ocupacion.'&Ocupacion='.$request->Ocupacion.'&Centro_Costos='.$request->Centro_Costos.'&Nivel_Salarial='.$request->Nivel_Salarial.'&EstadoTrabajador='.$request->EstadoTrabajador.'&Tipo_Contrato='.$request->Tipo_Contrato.'&Tipo_Cese='.$request->Tipo_Cese.'&EstadoCivil='.$request->EstadoCivil.'&TipodeComision='.$request->TipodeComision.'&FechaNacimiento='.$request->FechaNacimiento.'&FechaIngreso='.$request->FechaIngreso.'&FechaCese='.$request->FechaCese.'&PeriododeVacaciones='.$request->PeriododeVacaciones.'&FechaReingreso='.$request->FechaReingreso.'&Fecha_Ult_Actualizacion='.$request->Fecha_Ult_Actualizacion.'&EsReingreso='.$request->EsReingreso.'&Tipo_Cuenta='.$request->Tipo_Cuenta.'&FormaCalculo13ro='.$request->FormaCalculo13ro.'&FormaCalculo14ro='.$request->FormaCalculo14ro.'&BoniComplementaria='.$request->BoniComplementaria.'&BoniEspecial='.$request->BoniEspecial.'&Remuneracion_Minima='.$request->Remuneracion_Minima.'&Fondo_Reserva='.$request->Fondo_Reserva.'&Mensaje='.$request->Mensaje;
             $url = $apiURL . '/api/Varios/TrabajadorInsert?COMP_Codigo='.$request->COMP_Codigo.'&Tipo_trabajador='.$request->Tipo_trabajador.'&Apellido_Paterno='.$request->Apellido_Paterno.'&Apellido_Materno='.$request->Apellido_Materno.'&Nombres='.$request->Nombres.'&Identificacion='.$request->Identificacion.'&Entidad_Bancaria='.$request->Entidad_Bancaria.'&CarnetIESS='.$request->CarnetIESS.'&Direccion='.$request->Direccion.'&Telefono_Fijo='.$request->Telefono_Fijo.'&Telefono_Movil='.$request->Telefono_Movil.'&Genero='.$request->Genero.'&Nro_Cuenta_Bancaria='.$request->Nro_Cuenta_Bancaria.'&Codigo_Categoria_Ocupacion='.$request->Codigo_Categoria_Ocupacion.'&Ocupacion='.$request->Ocupacion.'&Centro_Costos='.$request->Centro_Costos.'&Nivel_Salarial='.$request->Nivel_Salarial.'&EstadoTrabajador='.$request->EstadoTrabajador.'&Tipo_Contrato='.$request->Tipo_Contrato.'&Tipo_Cese='.$request->Tipo_Cese.'&EstadoCivil='.$request->EstadoCivil.'&TipodeComision='.$request->TipodeComision.'&FechaNacimiento='.$request->FechaNacimiento.'T00:00:00&FechaIngreso='.$request->FechaIngreso.'T00:00:00&FechaCese='.$request->FechaCese.'T00:00:00&PeriododeVacaciones='.$request->PeriododeVacaciones.'&FechaReingreso='.$request->FechaReingreso.'T00:00:00&Fecha_Ult_Actualizacion='.$request->Fecha_Ult_Actualizacion.'T00:00:00&EsReingreso='.$request->EsReingreso.'&Tipo_Cuenta='.$request->Tipo_Cuenta.'&FormaCalculo13ro='.$request->FormaCalculo13ro.'&FormaCalculo14ro='.$request->FormaCalculo14ro.'&BoniComplementaria='.$request->BoniComplementaria.'&BoniEspecial='.$request->BoniEspecial.'&Remuneracion_Minima='.$request->Remuneracion_Minima.'&Fondo_Reserva='.$request->Fondo_Reserva.'&Mensaje='.$request->Mensaje;
-            //return $url;
             $response = Http::post($url);
             $response = $response->getBody();
-            //return $response;
-            return response()->json(['success' => 1, 'message' => $response], 200);
-        /* } catch (\Exception $th) {
+            $response = json_decode($response);
+            $msg = ($response == "Ingreso Exitoso") ? $response : 'Error al insertar';
+            return response()->json(['success' => 1, 'message' => $msg], 200);
+        } catch (\Exception $th) {
             return response()->json(['success' => 0, 'message' => $th], 201);
-        } */
+        }
     }
 
     public function deleteTrabajador(Request $request)
@@ -46,7 +45,9 @@ class TrabajadorController extends Controller
             
             $response = Http::get($url);
             $response = $response->getBody();
-            return $response;
+            $response = json_decode($response);
+            $msg = ($response == "Eliminación Exitosa") ? $response : 'Error al insertar';
+            return response()->json(['success' => 1, 'message' => $msg], 200);
         } catch (\Exception $th) {
             return response()->json(['success' => 0, 'message' => $th], 201);
         }
@@ -60,8 +61,9 @@ class TrabajadorController extends Controller
             $url = $apiURL.'/api/Varios/TrabajadorUpdate?COMP_Codigo='.$request->COMP_Codigo.'&Id_Trabajador='.$request->Id_Trabajador.'&Tipo_trabajador='.$request->Tipo_trabajador.'&Apellido_Paterno='.$request->Apellido_Paterno.'&Apellido_Materno='.$request->Apellido_Materno.'&Nombres='.$request->Nombres.'&Identificacion='.$request->Identificacion.'&Entidad_Bancaria='.$request->Entidad_Bancaria.'&CarnetIESS='.$request->CarnetIESS.'&Direccion='.$request->Direccion.'&Telefono_Fijo='.$request->Telefono_Fijo.'&Telefono_Movil='.$request->Telefono_Movil.'&Genero='.$request->Genero.'&Nro_Cuenta_Bancaria='.$request->Nro_Cuenta_Bancaria.'&Codigo_Categoria_Ocupacion='.$request->Codigo_Categoria_Ocupacion.'&Ocupacion='.$request->Ocupacion.'&Centro_Costos='.$request->Centro_Costos.'&Nivel_Salarial='.$request->Nivel_Salarial.'&EstadoTrabajador='.$request->EstadoTrabajador.'&Tipo_Contrato='.$request->Tipo_Contrato.'&Tipo_Cese='.$request->Tipo_Cese.'&EstadoCivil='.$request->EstadoCivil.'&TipodeComision='.$request->TipodeComision.'&FechaNacimiento='.$request->FechaNacimiento.'T00:00:00&FechaIngreso='.$request->FechaIngreso.'T00:00:00&FechaCese='.$request->FechaCese.'T00:00:00&PeriododeVacaciones='.$request->PeriododeVacaciones.'&FechaReingreso='.$request->FechaReingreso.'T00:00:00&Fecha_Ult_Actualizacion='.$request->Fecha_Ult_Actualizacion.'T00:00:00&EsReingreso='.$request->EsReingreso.'&Tipo_Cuenta='.$request->Tipo_Cuenta.'&FormaCalculo13ro='.$request->FormaCalculo13ro.'&FormaCalculo14ro='.$request->FormaCalculo14ro.'&BoniComplementaria='.$request->BoniComplementaria.'&BoniEspecial='.$request->BoniEspecial.'&Remuneracion_Minima='.$request->Remuneracion_Minima.'&Fondo_Reserva='.$request->Fondo_Reserva.'&Mensaje='.$request->Mensaje;
             //return $url;
             $response = Http::post($url);
-            $response = $response->getBody();
-            return response()->json(['success' => 1, 'message' => $response], 200);
+            $response = json_decode($response);
+            $msg = ($response == "Actualización  Exitosa") ? $response : 'Error al insertar';
+            return response()->json(['success' => 1, 'message' => $msg], 200);
         } catch (\Exception $th) {
             return response()->json(['success' => 0, 'message' => $th], 201);
         }
